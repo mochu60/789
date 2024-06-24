d76286f89fe6af9da9372948fa5d320a4af3b5ab
mochu60 <107395475+mochu60@users.noreply.github.com>
2024-06-24 22:39:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -163,14 +163,6 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             },
             "love_day": {
                 "value": love_days,
-                "color": get_color()
-            },
-            "note_en": {
-                "value": note_en,
-                "color": get_color()
-            },
-            "note_ch": {
-                "value": note_ch,
                 "color": get_color()
             }
         }
@@ -222,8 +214,6 @@ if __name__ == "__main__":
     # 传入地区获取天气信息
     region = config["region"]
     weather, temp, wind_dir = get_weather(region)
-    if note_ch == "" and note_en == "":
-        # 获取词霸每日金句
     # 公众号推送消息
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir)
