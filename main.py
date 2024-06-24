191b074ea75861a48d6423605645984156236b1a
mochu60 <107395475+mochu60@users.noreply.github.com>
2024-06-24 23:10:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -151,9 +151,9 @@ def send_message(to_user, access_token, region_name, weather, temp,wind_dir ):
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         else:
-            birthday_data = "{}的生日还有{}天".format(value["name"], birth_day)
-        # 将生日数据插入data
             birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+        # 将生日数据插入data
+        data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
         'Content-Type': 'application/json',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
