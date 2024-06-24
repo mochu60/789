c9becedce6ffa5cfaba09f169db2b91933e73644
mochu60 <107395475+mochu60@users.noreply.github.com>
2024-06-24 22:13:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,6 @@ def send_message(to_user, access_token, region_name, weather, temp ):
     for key, value in birthdays.items():
         # 获取距离下次生日的时间
         birth_day = get_birthday(value["birthday"], year, today)
-        print(f"生日数据: {key}, 距离生日还有: {birth_day}")  # 新增打印语句用于调试
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         else:
@@ -199,5 +198,5 @@ if __name__ == "__main__":
     for user in users:
         send_message(user, accessToken, region, weather, temp, wind_dir)
     os.system("pause")
-
-
+    
+    
